1092a3c40959deb0b84314a003361933c6cf7b54
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-11 10:59:16 +0100

    [Feature] Set up Recangle with 2 buttons for onLongPressGesture

--- SwiftfulThinkingContinuedLearning/ContentView.swift
+++ SwiftfulThinkingContinuedLearning/ContentView.swift
@@ -12,6 +12,8 @@ struct ContentView: View {
     @State var isComplete: Bool = false
     
     var body: some View {
+        
+        // Ex: 1 - Button - onLongPressGesture ---------------------------------
         Text(isComplete ? "Completed" : "Not Completed")
             .padding()
             .padding(.horizontal)
@@ -23,6 +25,28 @@ struct ContentView: View {
             ) {
                 isComplete.toggle()
             }
+        
+        // Ex: 2 - Retangle - 2 buttons - onLongPressGesture -------------------
+        VStack {
+            Rectangle()
+                .fill(Color.blue)
+                .frame(maxWidth: 10)
+                .frame(height: 55)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .background(Color.gray)
+            HStack {
+                Text("Click Here")
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.black)
+                    .cornerRadius(10)
+                Text("Reset")
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.black)
+                    .cornerRadius(10)
+            }
+        }
     }
 }
 
