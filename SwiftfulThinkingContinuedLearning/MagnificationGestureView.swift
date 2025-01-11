1f05b158163cc8de0389c2b493542d9b5d70346f
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-11 12:14:02 +0100

    [Chore] Create MagnificationGestureView file

---
+++ SwiftfulThinkingContinuedLearning/MagnificationGestureView.swift
@@ -0,0 +1,18 @@
+//
+//  MagnificationGestureView.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by Thomas on 1/11/25.
+//
+
+import SwiftUI
+
+struct MagnificationGestureView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MagnificationGestureView()
+}
