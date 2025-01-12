20adfad8c08d66e46c7758bfa768d3f5816a6b34
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-12 04:47:41 +0100

    [Chore] Create RotationGesture View File

---
+++ SwiftfulThinkingContinuedLearning/RotationGesture.swift
@@ -0,0 +1,18 @@
+//
+//  RotationGesture.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by Thomas on 1/12/25.
+//
+
+import SwiftUI
+
+struct RotationGesture: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RotationGesture()
+}
