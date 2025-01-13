be4380249ead500b2a84c5d39b6895e3cd9ad9dc
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-13 11:15:06 +0100

    [Feature] Add ScrollViewReader, anchored to #49

--- SwiftfulThinkingContinuedLearning/ScrollViewReaderBootcamp.swift
+++ SwiftfulThinkingContinuedLearning/ScrollViewReaderBootcamp.swift
@@ -10,15 +10,21 @@ import SwiftUI
 struct ScrollViewReaderBootcamp: View {
     var body: some View {
         ScrollView {
-            ForEach(0..<50) { index in
-                Text("This is item #\(index)")
-                    .font(.headline)
-                    .frame(height: 200)
-                    .frame(maxWidth: .infinity)
-                    .background(Color.white)
-                    .cornerRadius(10)
-                    .shadow(radius: 10)
-                    .padding()
+            ScrollViewReader { proxy in
+                Button("Click here to go to #49") {
+                    proxy.scrollTo(49, anchor: nil)
+                }
+                
+                ForEach(0..<50) { index in
+                    Text("This is item #\(index)")
+                        .font(.headline)
+                        .frame(height: 200)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .shadow(radius: 10)
+                        .padding()
+                }
             }
         }
     }
