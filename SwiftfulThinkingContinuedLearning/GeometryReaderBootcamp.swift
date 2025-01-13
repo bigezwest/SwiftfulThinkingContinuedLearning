a75d2f845d56b496133e3bc7e0ddf5544b8b33a6
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-13 11:35:29 +0100

    [Chore] Create GeometryReaderBootcamp View

---
+++ SwiftfulThinkingContinuedLearning/GeometryReaderBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  GeometryReaderBootcamp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by Thomas on 1/13/25.
+//
+
+import SwiftUI
+
+struct GeometryReaderBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GeometryReaderBootcamp()
+}
