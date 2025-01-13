1b14637445d3ecf821cbdebf8372fcf21c04d5e0
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-13 12:30:55 +0100

    [Feature] Set up View with 2 rectangles to work with

--- SwiftfulThinkingContinuedLearning/GeometryReaderBootcamp.swift
+++ SwiftfulThinkingContinuedLearning/GeometryReaderBootcamp.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct GeometryReaderBootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack (spacing: 0) {
+            Rectangle().fill(Color.red)
+            Rectangle().fill(Color.blue)
+        }
+        .ignoresSafeArea()
     }
 }
 
