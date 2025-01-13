d21bf0b0cda2ff3e31b50f27afe1f3279c23374f
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-13 20:00:59 +0100

    [Feature] Add OverlyView - Add GeometryReader, ZStack, Rectangle

--- SwiftfulThinkingContinuedLearning/MaskBootcamp.swift
+++ SwiftfulThinkingContinuedLearning/MaskBootcamp.swift
@@ -9,19 +9,31 @@ import SwiftUI
 
 struct MaskBootcamp: View {
     
-    @State var rating: Int = 4
+    @State var rating: Int = 0
     
     var body: some View {
         ZStack {
             starsView
+                .overlay(overlayView.mask(starsView))
         }
     }
+
+    private var overlayView: some View {
+        GeometryReader { geometry in
+            ZStack (alignment: .leading) {
+                Rectangle()
+                    .foregroundColor(.yellow)
+                    .frame(width: CGFloat(rating) / 5 * geometry.size.width)
+            }
+        }
+    }
+
     private var starsView: some View {
         HStack {
             ForEach(1..<6) { index in
                 Image(systemName: "star.fill")
                     .font(.largeTitle)
-                    .foregroundColor(rating >= index ? Color.yellow : Color.gray)
+                    .foregroundColor(Color.gray)
                     .onTapGesture { rating = index }
             }
         }
