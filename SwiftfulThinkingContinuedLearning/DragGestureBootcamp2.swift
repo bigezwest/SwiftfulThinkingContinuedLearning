0a421de189be75525bc60f89bbd1aee03b472574
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-13 10:52:53 +0100

    [Feature] Make MySignupView open to top once currentOffsetY is > 150

--- SwiftfulThinkingContinuedLearning/DragGestureBootcamp2.swift
+++ SwiftfulThinkingContinuedLearning/DragGestureBootcamp2.swift
@@ -8,17 +8,19 @@
 import SwiftUI
 
 struct DragGestureBootcamp2: View {
-    
+
     @State var startingOffsetY: CGFloat = UIScreen.main.bounds.height * 0.85
     @State var currentDragOffsetY: CGFloat = 0
-    
+    @State var endingOffsetY: CGFloat = 0
+
     var body: some View {
         ZStack {
             Color.green.ignoresSafeArea()
-            
+
             MySignupView()
                 .offset(y: startingOffsetY)
                 .offset(y: currentDragOffsetY)
+                .offset(y: endingOffsetY)
                 .gesture(
                     DragGesture()
                         .onChanged { value in
@@ -26,7 +28,18 @@ struct DragGestureBootcamp2: View {
                                 currentDragOffsetY = value.translation.height
                             }
                         }
-                    )
+                        .onEnded { value in
+                            withAnimation(.spring()) {
+                                if currentDragOffsetY < -150 {
+                                    endingOffsetY = -startingOffsetY
+                                    currentDragOffsetY = 0
+                                } else {
+                                    currentDragOffsetY = 0
+                                }
+                            }
+                        }
+                )
+            Text("\(currentDragOffsetY)")
         }
         .ignoresSafeArea(edges: .bottom)
     }
@@ -38,21 +51,23 @@ struct DragGestureBootcamp2: View {
 
 struct MySignupView: View {
     var body: some View {
-        VStack (spacing: 20) {
+        VStack(spacing: 20) {
             Image(systemName: "chevron.up")
                 .padding(.top)
             Text("Sign up")
                 .font(.headline)
                 .fontWeight(.semibold)
-            
+
             Image(systemName: "flame.fill")
                 .resizable()
                 .scaledToFit()
                 .frame(width: 100, height: 100)
-            
-            Text("This is the description of our app.  This is my favorite SwiftUI course and I recommend to all of my friends to subscribe to Swiftul Thinking. ")
-                .multilineTextAlignment(.center)
-            
+
+            Text(
+                "This is the description of our app.  This is my favorite SwiftUI course and I recommend to all of my friends to subscribe to Swiftul Thinking. "
+            )
+            .multilineTextAlignment(.center)
+
             Text("Create an Account")
                 .foregroundColor(.white)
                 .font(.headline)
@@ -60,7 +75,7 @@ struct MySignupView: View {
                 .padding(.horizontal)
                 .background(Color.black.cornerRadius(10))
             Spacer()
-            
+
         }
         .frame(maxWidth: .infinity)
         .background(Color.white)
