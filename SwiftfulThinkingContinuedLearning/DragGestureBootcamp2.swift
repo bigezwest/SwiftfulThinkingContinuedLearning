cae16f7197a91f8e64efdba6d29b25e3cf900ce5
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-13 10:12:03 +0100

    [Feature] Add file for new exmaple - DragGestureBootcamp2 view

---
+++ SwiftfulThinkingContinuedLearning/DragGestureBootcamp2.swift
@@ -0,0 +1,18 @@
+//
+//  DragGestureBootcamp2.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by Thomas on 1/13/25.
+//
+
+import SwiftUI
+
+struct DragGestureBootcamp2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DragGestureBootcamp2()
+}
