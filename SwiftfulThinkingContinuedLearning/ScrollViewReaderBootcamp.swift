d1039db726d6a711fefe9ffd93eab01883a93316
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-13 11:10:49 +0100

    [Feature] Add ScrollView and dummy content cards

--- SwiftfulThinkingContinuedLearning/ScrollViewReaderBootcamp.swift
+++ SwiftfulThinkingContinuedLearning/ScrollViewReaderBootcamp.swift
@@ -9,7 +9,18 @@ import SwiftUI
 
 struct ScrollViewReaderBootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            ForEach(0..<50) { index in
+                Text("This is item #\(index)")
+                    .font(.headline)
+                    .frame(height: 200)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.white)
+                    .cornerRadius(10)
+                    .shadow(radius: 10)
+                    .padding()
+            }
+        }
     }
 }
 
