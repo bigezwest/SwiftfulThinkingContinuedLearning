c3cc9765845f600b3ceac0a087f97da5cea4f9a4
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-13 10:38:25 +0100

    [Refactor] Extract Vstack to MySignupView subView

--- SwiftfulThinkingContinuedLearning/DragGestureBootcamp2.swift
+++ SwiftfulThinkingContinuedLearning/DragGestureBootcamp2.swift
@@ -12,33 +12,7 @@ struct DragGestureBootcamp2: View {
         ZStack {
             Color.green.ignoresSafeArea()
             
-            VStack (spacing: 20) {
-                Image(systemName: "chevron.up")
-                    .padding(.top)
-                Text("Sign up")
-                    .font(.headline)
-                    .fontWeight(.semibold)
-                
-                Image(systemName: "flame.fill")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 100, height: 100)
-                
-                Text("This is the description of our app.  This is my favorite SwiftUI course and I recommend to all of my friends to subscribe to Swiftul Thinking. ")
-                    .multilineTextAlignment(.center)
-                
-                 Text("Create an Account")
-                    .foregroundColor(.white)
-                    .font(.headline)
-                    .padding()
-                    .padding(.horizontal)
-                    .background(Color.black.cornerRadius(10))
-                Spacer()
-                
-            }
-            .frame(maxWidth: .infinity)
-            .background(Color.white)
-            .cornerRadius(30)
+            MySignupView()
         }
         .ignoresSafeArea(edges: .bottom)
     }
@@ -46,4 +20,36 @@ struct DragGestureBootcamp2: View {
 
 #Preview {
     DragGestureBootcamp2()
+}
+
+struct MySignupView: View {
+    var body: some View {
+        VStack (spacing: 20) {
+            Image(systemName: "chevron.up")
+                .padding(.top)
+            Text("Sign up")
+                .font(.headline)
+                .fontWeight(.semibold)
+            
+            Image(systemName: "flame.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height: 100)
+            
+            Text("This is the description of our app.  This is my favorite SwiftUI course and I recommend to all of my friends to subscribe to Swiftul Thinking. ")
+                .multilineTextAlignment(.center)
+            
+            Text("Create an Account")
+                .foregroundColor(.white)
+                .font(.headline)
+                .padding()
+                .padding(.horizontal)
+                .background(Color.black.cornerRadius(10))
+            Spacer()
+            
+        }
+        .frame(maxWidth: .infinity)
+        .background(Color.white)
+        .cornerRadius(30)
+    }
 }
