4d0fcb9e816ebefe3edd71bdd2738dadde25a7e3
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-13 17:51:31 +0100

    [Chore] Create MultipleSheetsBootcamp View

---
+++ SwiftfulThinkingContinuedLearning/MultipleSheetsBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  MultipleSheetsBootcamp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by Thomas on 1/13/25.
+//
+
+import SwiftUI
+
+struct MultipleSheetsBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MultipleSheetsBootcamp()
+}
