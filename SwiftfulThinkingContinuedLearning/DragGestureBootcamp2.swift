46bda837d9d5c14390ba56867c0f29ee4a7d2fa9
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-13 10:36:42 +0100

    [Feature] Set up example - sign up page

--- SwiftfulThinkingContinuedLearning/DragGestureBootcamp2.swift
+++ SwiftfulThinkingContinuedLearning/DragGestureBootcamp2.swift
@@ -9,7 +9,38 @@ import SwiftUI
 
 struct DragGestureBootcamp2: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.green.ignoresSafeArea()
+            
+            VStack (spacing: 20) {
+                Image(systemName: "chevron.up")
+                    .padding(.top)
+                Text("Sign up")
+                    .font(.headline)
+                    .fontWeight(.semibold)
+                
+                Image(systemName: "flame.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                
+                Text("This is the description of our app.  This is my favorite SwiftUI course and I recommend to all of my friends to subscribe to Swiftul Thinking. ")
+                    .multilineTextAlignment(.center)
+                
+                 Text("Create an Account")
+                    .foregroundColor(.white)
+                    .font(.headline)
+                    .padding()
+                    .padding(.horizontal)
+                    .background(Color.black.cornerRadius(10))
+                Spacer()
+                
+            }
+            .frame(maxWidth: .infinity)
+            .background(Color.white)
+            .cornerRadius(30)
+        }
+        .ignoresSafeArea(edges: .bottom)
     }
 }
 
