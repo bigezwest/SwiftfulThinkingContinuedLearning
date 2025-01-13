0a840e1c10d4d0305d6f23675968c4a7a5c12f63
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-13 11:26:26 +0100

    [Feature] Add TextField to accept user input for scrollto feature

--- SwiftfulThinkingContinuedLearning/ScrollViewReaderBootcamp.swift
+++ SwiftfulThinkingContinuedLearning/ScrollViewReaderBootcamp.swift
@@ -8,25 +8,41 @@
 import SwiftUI
 
 struct ScrollViewReaderBootcamp: View {
+    
+    @State var scrollToIndex: Int = 0
+    @State var textFieldText: String = ""
+    
     var body: some View {
-        ScrollView {
-            ScrollViewReader { proxy in
-                Button("Click here to go to #49") {
-                    withAnimation(.spring()) {
-                        proxy.scrollTo(49, anchor: nil)
+        VStack {
+            TextField("Enter a number here", text: $textFieldText)
+                .frame(height: 55)
+                .border(Color.gray)
+                .padding(.horizontal)
+                .keyboardType(.numberPad)
+            Button("Scroll Now") {
+                withAnimation(.spring()) {
+                    if let index = Int(textFieldText) {
+                        scrollToIndex = index
                     }
                 }
-                
-                ForEach(0..<50) { index in
-                    Text("This is item #\(index)")
-                        .font(.headline)
-                        .frame(height: 200)
-                        .frame(maxWidth: .infinity)
-                        .background(Color.white)
-                        .cornerRadius(10)
-                        .shadow(radius: 10)
-                        .padding()
-                        .id(index)
+            }
+            
+            ScrollView {
+                ScrollViewReader { proxy in
+                    ForEach(0..<50) { index in
+                        Text("This is item #\(index)")
+                            .font(.headline)
+                            .frame(height: 200)
+                            .frame(maxWidth: .infinity)
+                            .background(Color.white)
+                            .cornerRadius(10)
+                            .shadow(radius: 10)
+                            .padding()
+                            .id(index)
+                    }
+                    .onChange(of: scrollToIndex, perform: { value in
+                        proxy.scrollTo(value, anchor: nil)
+                    })
                 }
             }
         }
