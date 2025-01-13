0a1d71742f95908364a0c601835a0ccde05fa541
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-13 21:00:08 +0100

    [Feature] Add player, and playsound function to class Soundmanager - Add url - Add player - Add error message

--- SwiftfulThinkingContinuedLearning/SoundsBootcamp.swift
+++ SwiftfulThinkingContinuedLearning/SoundsBootcamp.swift
@@ -6,12 +6,23 @@
 //
 
 import SwiftUI
+import AVKit
 
 class SoundManager {
     // Make the soundManager available to use in any view in the app.
     static let instance = SoundManager()
-    
-    
+    // Var to hold AVPlayer
+    var player: AVAudioPlayer?
+    func playSound() {
+        
+        guard let url = URL(string: "") else { return }
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+        } catch let error {
+            print("Error playing sound \(error.localizedDescription)")
+        }
+    }
 }
 
 struct SoundsBootcamp: View {
