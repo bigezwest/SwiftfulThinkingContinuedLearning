6a227c624985170b74a807d3ff682bdf80f88d0c
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-13 19:50:21 +0100

    [Feature] Add @State var for rating - Change foreground color of star according to rating

--- SwiftfulThinkingContinuedLearning/MaskBootcamp.swift
+++ SwiftfulThinkingContinuedLearning/MaskBootcamp.swift
@@ -8,13 +8,16 @@
 import SwiftUI
 
 struct MaskBootcamp: View {
+    
+    @State var rating: Int = 3
+    
     var body: some View {
         ZStack {
             HStack {
                 ForEach(1..<6) { index in
                     Image(systemName: "star.fill")
                         .font(.largeTitle)
-                        .foregroundColor(.gray)
+                        .foregroundColor(rating >= index ? Color.yellow : Color.gray)
                 }
             }
         }
