f07903206039a4156c2c08a8238d93d9c15f0117
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-13 09:58:39 +0100

    [Feature] Set up example 2 - A card that can be swiped

--- SwiftfulThinkingContinuedLearning/DragGestureBootcamp.swift
+++ SwiftfulThinkingContinuedLearning/DragGestureBootcamp.swift
@@ -14,22 +14,43 @@ struct DragGestureBootcamp: View {
     var body: some View {
         
         // Ex: 1 - A square that has DragGesture attached to it ----------------
-        RoundedRectangle(cornerRadius: 20)
-            .frame(width: 100, height: 100)
-            .offset(offset)
-            .gesture(
-                DragGesture()
-                    .onChanged { value in
-                        withAnimation(.spring()) {
-                            offset = value.translation
+//        RoundedRectangle(cornerRadius: 20)
+//            .frame(width: 100, height: 100)
+//            .offset(offset)
+//            .gesture(
+//                DragGesture()
+//                    .onChanged { value in
+//                        withAnimation(.spring()) {
+//                            offset = value.translation
+//                        }
+//                    }
+//                    .onEnded { value in
+//                        withAnimation(.spring()) {
+//                            offset = .zero
+//                        }
+//                    }
+//            )
+        
+    // Ex: 2 - A card that can be swiped ---------------------------------------
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: 300, height: 500)
+                .offset(offset)
+                .gesture(
+                    DragGesture()
+                        .onChanged { value in
+                            withAnimation(.spring()) {
+                                offset = value.translation
+                            }
                         }
-                    }
-                    .onEnded { value in
-                        withAnimation(.spring()) {
-                            offset = .zero
+                        .onEnded { value in
+                            withAnimation(.spring()) {
+                                offset = .zero
+                            }
                         }
-                    }
-            )
+                )
+        }
+
     }
 
 }
