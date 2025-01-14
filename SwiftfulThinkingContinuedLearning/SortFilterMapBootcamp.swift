26e6ad5830b21d5ec7a01861aea470857785af0a
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-14 11:07:18 +0100

    [Feature] Add dummy data to getUsers() for 10 users

--- SwiftfulThinkingContinuedLearning/SortFilterMapBootcamp.swift
+++ SwiftfulThinkingContinuedLearning/SortFilterMapBootcamp.swift
@@ -20,7 +20,16 @@ class ArrayModificationVewModel: ObservableObject {
     }
     
     func getUsers() {
-        
+        let user1 = UserModel(name: "Thomas", points: 100, isVerified: true)
+        let user2 = UserModel(name: "Dick", points: 200, isVerified: false)
+        let user3 = UserModel(name: "Harry", points: 300, isVerified: true)
+        let user4 = UserModel(name: "Joe", points: 400, isVerified: false)
+        let user5 = UserModel(name: "Sue", points: 500, isVerified: true)
+        let user6 = UserModel(name: "Fred", points: 600, isVerified: false)
+        let user7 = UserModel(name: "Barney", points: 700, isVerified: true)
+        let user8 = UserModel(name: "Wilma", points: 800, isVerified: false)
+        let user9 = UserModel(name: "Betty", points: 900, isVerified: true)
+        let user10 = UserModel(name: "Dr. DoLittle", points: 1000, isVerified: false)
     }
 }
 
