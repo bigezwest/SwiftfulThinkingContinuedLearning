4dac1ad8eeae22169ed49eaca2255b72a015e26c
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-14 03:35:54 +0100

    [Chore] Create LocalNotificationsBootcamp View

---
+++ SwiftfulThinkingContinuedLearning/LocalNotificationsBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  LocalNotificationsBootcamp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by Thomas on 1/14/25.
+//
+
+import SwiftUI
+
+struct LocalNotificationsBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocalNotificationsBootcamp()
+}
