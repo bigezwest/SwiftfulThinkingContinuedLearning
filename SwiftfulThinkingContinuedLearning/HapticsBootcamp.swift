0665ac17f6243f7b3c2f4eed87a8e47ee71f38f6
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-14 03:18:52 +0100

    [Feature] Add generator functionality to notification() and impact()

--- SwiftfulThinkingContinuedLearning/HapticsBootcamp.swift
+++ SwiftfulThinkingContinuedLearning/HapticsBootcamp.swift
@@ -10,12 +10,14 @@ import SwiftUI
 class HapticManager {
     static let instance = HapticManager() // Singleton
     
-    func notification() {
-        
+    func notification(type: UINotificationFeedbackGenerator.FeedbackType) {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(type)
     }
     
-    func impact() {
-        
+    func impact(style: UIImpactFeedbackGenerator.FeedbackStyle) {
+        let generator = UIImpactFeedbackGenerator(style: style)
+        generator.impactOccurred()
     }
 }
 
