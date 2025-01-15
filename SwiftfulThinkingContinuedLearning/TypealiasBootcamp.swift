a4935f9a5ef986da9907a5442310780fbceb606e
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-15 17:52:14 +0100

    [Chore] Create TypealiasBootcamp View

---
+++ SwiftfulThinkingContinuedLearning/TypealiasBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  TypealiasBootcamp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by Thomas on 1/15/25.
+//
+
+import SwiftUI
+
+struct TypealiasBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TypealiasBootcamp()
+}
