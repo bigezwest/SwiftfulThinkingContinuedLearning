d2c8d03fb098553263b40a9f468d97372899c36a
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-15 17:53:57 +0100

    [Feature] Add MovieModel

--- SwiftfulThinkingContinuedLearning/TypealiasBootcamp.swift
+++ SwiftfulThinkingContinuedLearning/TypealiasBootcamp.swift
@@ -7,6 +7,12 @@
 
 import SwiftUI
 
+struct MovieModel {
+    let title: String
+    let director: String
+    let count: Int
+}
+
 struct TypealiasBootcamp: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
