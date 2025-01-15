67857af330e912c3f5ed0025326567db494d3773
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-15 06:27:53 +0100

    [Chore] Set up for coredata relationships

--- SwiftfulThinkingContinuedLearning/CoreDataRelationshipsBootcamp.swift
+++ SwiftfulThinkingContinuedLearning/CoreDataRelationshipsBootcamp.swift
@@ -6,8 +6,37 @@
 //
 
 import SwiftUI
+import CoreData
 
+class CoreDataManager {
+    static let instance = CoreDataManager()
+    let container: NSPersistentContainer
+    let context: NSManagedObjectContext
+    
+    init() {
+        container = NSPersistentContainer(name: "")
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Error loading core data \(error)")
+            }
+        }
+        context = container.viewContext
+    }
+    func save() {
+        do {
+            try context.save()
+        } catch let error {
+            print("Error Saving Core Data: \(error.localizedDescription)")
+        }
+    }
+}
+class CoreDataRelationshipViewModel: ObservableObject {
+    let manager = CoreDataManager.instance
+}
 struct CoreDataRelationshipsBootcamp: View {
+    
+    @StateObject var vm = CoreDataViewModel()
+
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
