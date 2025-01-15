84ead3f38bc86d6df954636f776e504d69e46fda
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-15 03:00:39 +0100

    [Chore] Create CoreDataBootcamp View

---
+++ SwiftfulThinkingContinuedLearning/CoreDataBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  CoreDataBootcamp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by Thomas on 1/15/25.
+//
+
+import SwiftUI
+
+
+
+struct CoreDataBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoreDataBootcamp()
+}
