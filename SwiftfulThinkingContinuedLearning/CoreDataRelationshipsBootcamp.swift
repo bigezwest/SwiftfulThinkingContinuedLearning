b9d29fc129ab4a7f45e87987c9e13fcf9a43cfc2
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-15 06:21:38 +0100

    [Chore] Create CoreDataRelationshipsBootcamp View

---
+++ SwiftfulThinkingContinuedLearning/CoreDataRelationshipsBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  CoreDataRelationshipsBootcamp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by Thomas on 1/15/25.
+//
+
+import SwiftUI
+
+struct CoreDataRelationshipsBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoreDataRelationshipsBootcamp()
+}
