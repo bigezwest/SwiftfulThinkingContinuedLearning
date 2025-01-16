c0ee204bca00e4fdd5b6b5afb5fbc7d9a5ec7fc3
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-16 04:33:32 +0100

    [Chore] Creaate CodableBootcamp View

---
+++ SwiftfulThinkingContinuedLearning/CodableBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  CodableBootcamp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by Thomas on 1/16/25.
+//
+
+import SwiftUI
+
+struct CodableBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CodableBootcamp()
+}
