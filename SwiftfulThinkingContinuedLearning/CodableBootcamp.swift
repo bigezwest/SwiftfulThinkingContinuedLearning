75c3fc0739b58d98e99d1ae5f542686efb1380b1
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-16 05:08:42 +0100

    [Feature] Create CodableViewModel Class - Add instance to View - Add CustomerModel - Add Sample data to CodableViewModel

--- SwiftfulThinkingContinuedLearning/CodableBootcamp.swift
+++ SwiftfulThinkingContinuedLearning/CodableBootcamp.swift
@@ -7,7 +7,26 @@
 
 import SwiftUI
 
+struct CustomerModel: Identifiable {
+    let id: String
+    let name: String
+    let points: Int
+    let isPremium: Bool
+}
+
+class CodableViewModel: ObservableObject {
+    @Published var customer: CustomerModel? = CustomerModel(
+        id: "1",
+        name: "Tom",
+        points: 5,
+        isPremium: true
+    )
+}
+
 struct CodableBootcamp: View {
+    
+    @StateObject var vm = CodableViewModel()
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
