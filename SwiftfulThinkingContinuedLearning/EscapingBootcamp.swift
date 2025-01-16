10748355a3f1168657e226a6d1e77f4f204519e7
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-16 03:39:47 +0100

    [Chore] Create EscapingBootcamp VIew

---
+++ SwiftfulThinkingContinuedLearning/EscapingBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  EscapingBootcamp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by Thomas on 1/16/25.
+//
+
+import SwiftUI
+
+struct EscapingBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EscapingBootcamp()
+}
