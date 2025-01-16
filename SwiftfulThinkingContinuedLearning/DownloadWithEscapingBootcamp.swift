2f1d85848bd4d9812b74cb0701034328829282bd
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-16 09:48:29 +0100

    [Chore] Create DownloadWithEscapingBootcamp

---
+++ SwiftfulThinkingContinuedLearning/DownloadWithEscapingBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  DownloadWithEscapingBootcamp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by Thomas on 1/16/25.
+//
+
+import SwiftUI
+
+struct DownloadWithEscapingBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DownloadWithEscapingBootcamp()
+}
