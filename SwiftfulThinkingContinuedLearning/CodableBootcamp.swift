b5cc11e70aecea0c7285fc73ac2a01b0265a2917
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-16 06:10:00 +0100

    [Feature] Implement Decodable - Add enum CodingKeys - Add init()

--- SwiftfulThinkingContinuedLearning/CodableBootcamp.swift
+++ SwiftfulThinkingContinuedLearning/CodableBootcamp.swift
@@ -7,23 +7,46 @@
 
 import SwiftUI
 
-struct CustomerModel: Identifiable {
+struct CustomerModel: Identifiable, Decodable {
     let id: String
     let name: String
     let points: Int
     let isPremium: Bool
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case points
+        case isPremium
+    }
+    
+    init(id: String, name: String, points: Int, isPremium: Bool) {
+        self.id = id
+        self.name = name
+        self.points = points
+        self.isPremium = isPremium
+    }
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(String.self, forKey: .id)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.points = try container.decode(Int.self, forKey: .points)
+        self.isPremium = try container.decode(Bool.self, forKey: .isPremium)
+    }
 }
 
 class CodableViewModel: ObservableObject {
     @Published var customer: CustomerModel? = nil
+    
     init() {
         getData()
     }
+    
     func getData() {
         guard let data = getJSONData() else { return }
-        print("JSON Data: ")
-        print(data)
+        self.customer = try? JSONDecoder().decode(CustomerModel.self, from: data)
     }
+
     func getJSONData() -> Data? {
         
         let dictionary: [String: Any] = [
