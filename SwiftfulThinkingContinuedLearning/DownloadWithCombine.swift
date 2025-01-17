57fee223cbe0a1c450c5e36776432a5d24fa6d51
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-17 04:04:09 +0100

    [Chore] Create DownloadWithCombine SwiftUI View

---
+++ SwiftfulThinkingContinuedLearning/DownloadWithCombine.swift
@@ -0,0 +1,18 @@
+//
+//  DownloadWithCombine.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by Thomas on 1/17/25.
+//
+
+import SwiftUI
+
+struct DownloadWithCombine: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DownloadWithCombine()
+}
