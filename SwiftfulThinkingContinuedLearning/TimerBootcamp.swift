407dc79f44a13f5cb6b868fd5ddd6a33e51a4ad8
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-17 11:01:21 +0100

    [Feature] Set up basic View

--- SwiftfulThinkingContinuedLearning/TimerBootcamp.swift
+++ SwiftfulThinkingContinuedLearning/TimerBootcamp.swift
@@ -9,7 +9,18 @@ import SwiftUI
 
 struct TimerBootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            RadialGradient(
+                gradient: Gradient(colors: [Color(#colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1)), Color(#colorLiteral(red: 0.004859850742, green: 0.09608627111, blue: 0.5749928951, alpha: 1))]),
+                center: .center,
+                startRadius: 5,
+                endRadius: 500)
+            Text("Hi")
+                .font(.system(size: 100, weight: .semibold, design: .rounded))
+                .foregroundColor(.white)
+                .lineLimit(1)
+                .minimumScaleFactor(0.1)
+        }
     }
 }
 
