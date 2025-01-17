7692dd943501f800f73f193148e1244d20988d6b
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-17 19:37:03 +0100

    [Feature] Add Save button

--- SwiftfulThinkingContinuedLearning/FileManagerBootcamp.swift
+++ SwiftfulThinkingContinuedLearning/FileManagerBootcamp.swift
@@ -32,6 +32,18 @@ struct FileManagerBootcamp: View {
                         .clipped()
                         .cornerRadius(10)
                 }
+                Button {
+                    
+                } label: {
+                    Text("Save to FM")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .padding()
+                        .padding(.horizontal)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                }
+
                 Spacer()
             }
             .navigationTitle("File Manager")
