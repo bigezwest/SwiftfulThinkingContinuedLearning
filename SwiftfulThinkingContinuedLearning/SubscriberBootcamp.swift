0a128f8547ebf9d5d6531eb54361d38980802bca
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-17 17:48:35 +0100

    [Feature] Set up project - Add SubscriberViewModel class with @Published var count - Add @StateObjectd vm - Add VStack to display count - Add timer var set to setUpTimer() value - Add setUpTimer(), add functionality - Add init() to class SubscriberViewModel and call setUpTimer()

--- SwiftfulThinkingContinuedLearning/SubscriberBootcamp.swift
+++ SwiftfulThinkingContinuedLearning/SubscriberBootcamp.swift
@@ -6,10 +6,36 @@
 //
 
 import SwiftUI
+import Combine
+
+class SubscriberViewModel: ObservableObject {
+    
+    @Published var count: Int = 0
+    var timer: AnyCancellable?
+    
+    init() {
+        setUpTimer()
+    }
+    
+    func setUpTimer() {
+        timer = Timer
+            .publish(every: 1, on: .main, in: .common)
+            .autoconnect()
+            .sink { [weak self] _ in
+                self?.count += 1
+            }
+    }
+}
 
 struct SubscriberBootcamp: View {
+    
+    @StateObject var vm = SubscriberViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("\(vm.count)")
+                .font(.largeTitle)
+        }
     }
 }
 
