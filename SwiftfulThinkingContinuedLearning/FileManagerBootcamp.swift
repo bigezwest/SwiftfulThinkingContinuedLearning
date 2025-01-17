7871aae606589ab336fd0f9368d6c255621f2e52
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-17 19:30:16 +0100

    [Feature] Add content - Add image, style - Add navigationTitle

--- SwiftfulThinkingContinuedLearning/FileManagerBootcamp.swift
+++ SwiftfulThinkingContinuedLearning/FileManagerBootcamp.swift
@@ -9,7 +9,19 @@ import SwiftUI
 
 struct FileManagerBootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                Image("steve-jobs")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 200, height: 200)
+                    .clipped()
+                    .cornerRadius(10)
+                Spacer()
+            }
+            .navigationTitle("File Manager")
+            
+        }
     }
 }
 
