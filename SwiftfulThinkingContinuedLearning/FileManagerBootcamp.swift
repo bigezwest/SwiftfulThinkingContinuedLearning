7da8661fd8921c285297e57c0f2b20e6d71c2c3d
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-17 19:35:24 +0100

    [Feature] Add ViewModel OO - Add vm State - Add check if image exists to View - var for imagename - Add init(), call getImageFromAssetsFolder - Add getImageFromAssetsFolder()

--- SwiftfulThinkingContinuedLearning/FileManagerBootcamp.swift
+++ SwiftfulThinkingContinuedLearning/FileManagerBootcamp.swift
@@ -8,15 +8,30 @@
 import SwiftUI
 
 struct FileManagerBootcamp: View {
+    
+    class FileManagerViewModel: ObservableObject {
+        @Published var image: UIImage? = nil
+        let imageName: String = "steve-jobs"
+        init() {
+            getImageFromAssetsFolder()
+        }
+        func getImageFromAssetsFolder() {
+            image = UIImage(named: imageName)
+        }
+    }
+    
     var body: some View {
+        @State var vm = FileManagerViewModel()
         NavigationView {
             VStack {
-                Image("steve-jobs")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 200, height: 200)
-                    .clipped()
-                    .cornerRadius(10)
+                if let image = vm.image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 200, height: 200)
+                        .clipped()
+                        .cornerRadius(10)
+                }
                 Spacer()
             }
             .navigationTitle("File Manager")
