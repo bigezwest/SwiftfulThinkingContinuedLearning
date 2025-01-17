8769ed441b4b2e79978eb7b3a77bf0a4af3f5146
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-17 04:32:03 +0100

    [Feature] Create DownloadWithCombineViewModel OO - Initialize in View

--- SwiftfulThinkingContinuedLearning/DownloadWithCombine.swift
+++ SwiftfulThinkingContinuedLearning/DownloadWithCombine.swift
@@ -14,7 +14,16 @@ struct PostModel: Identifiable, Codable {
     let body: String
 }
 
+class DownloadWithCombineViewModel: ObservableObject {
+    
+    @Published var posts: [PostModel] = []
+    
+}
+
 struct DownloadWithCombine: View {
+    
+    @StateObject var vm = DownloadWithCombineViewModel()
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
