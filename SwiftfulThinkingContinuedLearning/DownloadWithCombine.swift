bdd9e738d0ebcec9b7a6e54c8105c6b0b2d070bd
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-17 04:28:56 +0100

    [Feature] Create PostModel

--- SwiftfulThinkingContinuedLearning/DownloadWithCombine.swift
+++ SwiftfulThinkingContinuedLearning/DownloadWithCombine.swift
@@ -7,6 +7,13 @@
 
 import SwiftUI
 
+struct PostModel: Identifiable, Codable {
+    let userID: Int
+    let id: Int
+    let title: String
+    let body: String
+}
+
 struct DownloadWithCombine: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
