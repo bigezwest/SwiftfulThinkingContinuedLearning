36eb4128043f5496781df8394a399242d5e6b539
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-17 19:23:27 +0100

    [Chore] Create FileManagerBootcamp SwiftUi View

---
+++ SwiftfulThinkingContinuedLearning/FileManagerBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  FileManagerBootcamp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by Thomas on 1/17/25.
+//
+
+import SwiftUI
+
+struct FileManagerBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FileManagerBootcamp()
+}
