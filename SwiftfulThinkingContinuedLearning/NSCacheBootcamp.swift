f227326da2e33258d502c7b82d40ee4fe93869d9
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-19 05:46:46 +0100

    [Feature] Iniitial View setup - Add content to View - Add class CacheViewModel

--- SwiftfulThinkingContinuedLearning/NSCacheBootcamp.swift
+++ SwiftfulThinkingContinuedLearning/NSCacheBootcamp.swift
@@ -7,9 +7,62 @@
 
 import SwiftUI
 
+class CacheViewModel: ObservableObject {
+    
+    @Published var startingImage: UIImage? = nil
+    let imageName: String = "steve-jobs"
+    init() {
+        getImageFromAsssetsFolder()
+    }
+    func getImageFromAsssetsFolder () {
+        startingImage = UIImage(named: imageName)
+    }
+    
+}
+
+
 struct NSCacheBootcamp: View {
+    
+    @StateObject var vm = CacheViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                if let image = vm.startingImage {
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 200, height: 200)
+                        .clipped()
+                        .cornerRadius(10)
+                }
+                HStack {
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("Save to cache")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                    })
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("Delete from cache")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(Color.red)
+                            .cornerRadius(10)
+                    })
+                }
+
+                Spacer()
+            }
+            .navigationTitle(Text("NSCache Bootcamp"))
+        }
     }
 }
 
