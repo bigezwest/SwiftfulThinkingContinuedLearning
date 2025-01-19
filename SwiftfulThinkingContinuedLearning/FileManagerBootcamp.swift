866e89a401e52f6b1d1472cd6f2f53c022aed9a1
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-19 05:23:09 +0100

    [Feature] Delete item from FileManager - Add deleteImage() - Add deleteImage() and call in VIewModel - Add delete button and functionality to View - Display feedback in View

--- SwiftfulThinkingContinuedLearning/FileManagerBootcamp.swift
+++ SwiftfulThinkingContinuedLearning/FileManagerBootcamp.swift
@@ -8,21 +8,70 @@
 import SwiftUI
 
 class LocalFileManager {
+    
     static let instance = LocalFileManager()
-    func saveImage(image: UIImage, name: String) {
-        guard let data = image.jpegData(compressionQuality: 1.0),
-        let path = getPathForImage(name: name) else {
-            print("Error getting data.")
+    let folderName = "MyApp_Images"
+    
+    init() {
+        createFolderIfNeeded()
+    }
+    
+    func createFolderIfNeeded() {
+        guard
+            let path = FileManager
+                .default
+                .urls(for: .cachesDirectory, in: .userDomainMask)
+                .first?
+                .appendingPathComponent(folderName)
+                .path else {
             return
         }
-        
-        do {
-            try data.write(to: path)
-            print("Success Saving Image")
-        } catch let error {
-            print("Error saving \(error)")
+        if !FileManager.default.fileExists(atPath: path) {
+            do {
+                try FileManager.default
+                    .createDirectory(
+                        atPath: path,
+                        withIntermediateDirectories: true,
+                        attributes: nil)
+                print("Success creeating folder")
+            } catch let error {
+                print("Error creating folder. \(error)")
+            }
         }
     }
+    func deleteFolder() {
+        guard
+            let path = FileManager
+                .default
+                .urls(for: .cachesDirectory, in: .userDomainMask)
+                .first?
+                .appendingPathComponent(folderName)
+                .path else {
+            return
+        }
+        do {
+            try FileManager.default.removeItem(atPath: path)
+            print("Success deleting folder")
+        } catch let erorr {
+            print("Error deleting folder")
+        }
+    }
+    func saveImage(image: UIImage, name: String) -> String {
+        guard
+            let data = image.jpegData(compressionQuality: 1.0),
+            let path = getPathForImage(name: name) else {
+            return "Error getting data."
+        }
+
+        do {
+            try data.write(to: path)
+            print(path)
+            return "Success Saving Image"
+        } catch let error {
+            return "Error saving \(error)"
+        }
+    }
+    
     func getImage(name: String) -> UIImage? {
         guard
             let path = getPathForImage(name: name)?.path,
@@ -32,28 +81,49 @@ class LocalFileManager {
         }
         return UIImage(contentsOfFile: path)
     }
-
+    
+    func deleteImage(name: String) -> String {
+        guard
+            let path = getPathForImage(name: name)?.path,
+            FileManager.default.fileExists(atPath: path) else {
+            return "Error getting path"
+        }
+        do {
+            try FileManager.default.removeItem(atPath: path)
+            return "Successfully deleted"
+        } catch let error {
+            return "Error deleting image: \(error)"
+        }
+    }
+    
     func getPathForImage(name: String) -> URL? {
-        guard let path = FileManager
-            .default
-            .urls(for: .cachesDirectory, in: .userDomainMask)
-            .first?
-            .appendingPathComponent("\(name).jpg") else {
+        guard
+            let path = FileManager
+                .default
+                .urls(for: .cachesDirectory, in: .userDomainMask)
+                .first?
+                .appendingPathComponent(folderName)
+                .appendingPathComponent("\(name).jpg") else {
             print("Error getting path.")
             return nil
         }
         return path
     }
+    
 }
 
 class FileManagerViewModel: ObservableObject {
+
     @Published var image: UIImage? = nil
+
     let imageName: String = "steve-jobs"
     let manager = LocalFileManager.instance
-    
+
+    @Published var infoMessage: String = ""
+
     init() {
-//        getImageFromAssetsFolder()
-        getImageFromFileManager()
+        getImageFromAssetsFolder()
+//                getImageFromFileManager()
     }
     func getImageFromAssetsFolder() {
         image = UIImage(named: imageName)
@@ -63,14 +133,19 @@ class FileManagerViewModel: ObservableObject {
     }
     func saveImage() {
         guard let image = image else { return }
-        manager.saveImage(image: image, name: imageName)
+        infoMessage = manager.saveImage(image: image, name: imageName)
+    }
+    func deleteImage() {
+        infoMessage = manager.deleteImage(name: imageName)
+        manager.deleteFolder()
     }
 }
 
 struct FileManagerBootcamp: View {
-        
+    @StateObject var vm = FileManagerViewModel()
+    
     var body: some View {
-        @State var vm = FileManagerViewModel()
+
         NavigationView {
             VStack {
                 if let image = vm.image {
@@ -81,22 +156,38 @@ struct FileManagerBootcamp: View {
                         .clipped()
                         .cornerRadius(10)
                 }
-                Button {
-                    vm.saveImage()
-                } label: {
-                    Text("Save to FM")
-                        .foregroundColor(.white)
-                        .font(.headline)
-                        .padding()
-                        .padding(.horizontal)
-                        .background(Color.blue)
-                        .cornerRadius(10)
+                HStack {
+                    Button(action: {
+                        vm.saveImage()
+                    }, label: {
+                        Text("Save to FM")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                            .padding()
+                            .padding(.horizontal)
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                        })
+                    Button(action: {
+                        vm.deleteImage()
+                    }, label: {
+                        Text("Delete from FM")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                            .padding()
+                            .padding(.horizontal)
+                            .background(Color.red)
+                            .cornerRadius(10)
+                    })
                 }
-
+                Text(vm.infoMessage)
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.purple)
                 Spacer()
             }
             .navigationTitle("File Manager")
-            
+
         }
     }
 }
