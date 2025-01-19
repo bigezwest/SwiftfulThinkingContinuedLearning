38cb787b3f61ce905dfb6dd96a1d0fd2f0348c46
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-19 11:08:19 +0100

    [Chore] Create DownloadingImagesBootcamp SwiftUI View

---
+++ SwiftfulThinkingContinuedLearning/DownloadingImages/Views/DownloadingImagesBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  DownloadingImagesBootcamp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by Thomas on 1/19/25.
+//
+
+import SwiftUI
+
+struct DownloadingImagesBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DownloadingImagesBootcamp()
+}
