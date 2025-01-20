078535398486185ecce8c79f07f4e6e1c2476180
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-20 12:16:10 +0100

    [Feature] View setup

--- SwiftfulThinkingContinuedLearning/AccessibilityVoiceOver.swift
+++ SwiftfulThinkingContinuedLearning/AccessibilityVoiceOver.swift
@@ -8,8 +8,71 @@
 import SwiftUI
 
 struct AccessibilityVoiceOver: View {
+
+    @State private var isActive: Bool = false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Form {
+                Section {
+                    Toggle("Volume", isOn: $isActive)
+                    HStack {
+                        Text("Volume")
+                        Spacer()
+                        Text(isActive ? "On" : "OFF")
+                    }
+                    .background(Color.black.opacity(0.001))
+                    .onTapGesture {
+                        isActive.toggle()
+                    }
+
+                } header: {
+                    Text("PREFERENCES")
+                }
+
+                Section {
+                    Button("Favorites") {
+
+                    }
+                    Button {
+
+                    } label: {
+                        Image(systemName: "heart.fill")
+                    }
+                    Text("Favorites")
+                        .onTapGesture {
+
+                        }
+
+                } header: {
+                    Text("APPLICATION")
+                }
+                VStack {
+                    Text("CONTENT")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .foregroundColor(.secondary)
+                        .font(.caption)
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(spacing: 8) {
+                            ForEach(0..<10) { x in
+                                VStack {
+                                    Image("steve-jobs")
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width:100, height: 100)
+                                        .cornerRadius(10)
+                                    Text("Item \(x)")
+                                }
+                                .onTapGesture {
+                                    
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Settings")
+        }
     }
 }
 
