f01b21a9a281f2d7b383d8b74e3a36f05713f932
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-20 07:55:40 +0100

    [Chore] Create AccessibilityVoiceOver SwiftUI View

---
+++ SwiftfulThinkingContinuedLearning/AccessibilityVoiceOver.swift
@@ -0,0 +1,18 @@
+//
+//  AccessibilityVoiceOver.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by Thomas on 1/20/25.
+//
+
+import SwiftUI
+
+struct AccessibilityVoiceOver: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AccessibilityVoiceOver()
+}
