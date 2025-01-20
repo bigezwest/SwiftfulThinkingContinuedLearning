574580a02a9203554d58c857b62ccfe4cab497d3
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-20 21:08:10 +0100

    [Feature] Add example

--- SwiftfulThinkingContinuedLearning/AlignmentGuideBootcamp.swift
+++ SwiftfulThinkingContinuedLearning/AlignmentGuideBootcamp.swift
@@ -6,13 +6,44 @@
 //
 
 import SwiftUI
+//
+//struct AlignmentGuideBootcamp: View {
+//    var body: some View {
+//        
+//    }
+//}
 
-struct AlignmentGuideBootcamp: View {
+struct AlignmentChildView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 20) {
+            VStack (alignment: .leading, spacing: 20) {
+                row(title: "Row 1", showIcon: false)
+                row(title: "Row 2", showIcon: true)
+                row(title: "Row 3", showIcon: false)
+            }
+            .padding(16)
+            .background(Color.white)
+            .cornerRadius(10)
+            .shadow(radius: 10)
+            .padding(40)
+        }
+    }
+    
+    private func row(title: String, showIcon: Bool) -> some View {
+        HStack(spacing: 10) {
+            if showIcon {
+                Image(systemName: "info.circle")
+                    .frame(width: 30, height: 30)
+            }
+            Text(title)
+        
+            Spacer()
+        }
+        .alignmentGuide(.leading) { dimensions in
+            return showIcon ? 40 : 0
+        }
     }
 }
-
 #Preview {
-    AlignmentGuideBootcamp()
+    AlignmentChildView()
 }
