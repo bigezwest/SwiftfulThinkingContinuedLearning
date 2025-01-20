be723d50b4f74513d14e5d892fad855b75eab17c
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-20 07:50:17 +0100

    [Feature] Add Accessibility Colors

--- SwiftfulThinkingContinuedLearning/AccessibilityColorsBootcamp.swift
+++ SwiftfulThinkingContinuedLearning/AccessibilityColorsBootcamp.swift
@@ -8,8 +8,51 @@
 import SwiftUI
 
 struct AccessibilityColorsBootcamp: View {
+    
+    @Environment(\.accessibilityReduceTransparency) var reduceTransparency
+    @Environment(\.colorSchemeContrast) var colorSchemeContrast
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWIthoutColor
+    @Environment(\.accessibilityInvertColors) var invertColors
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Button("Button 1") {
+                    
+                }
+                .foregroundColor(colorSchemeContrast == .increased ? .white : .primary)
+                .buttonStyle(.borderedProminent)
+                
+                Button("Button 2") {
+                    
+                }
+                .foregroundColor(.primary)
+                .buttonStyle(.borderedProminent)
+                .tint(.orange)
+                
+                Button("Button 3") {
+                    
+                }
+                .foregroundColor(.white)
+                .foregroundColor(.primary)
+                .buttonStyle(.borderedProminent)
+                .tint(.green)
+                
+                Button("Button 4") {
+                    
+                }
+                .foregroundColor(.green)
+                .foregroundColor(.primary)
+                .buttonStyle(.borderedProminent)
+                .tint(differentiateWIthoutColor ? .black : .purple)
+                
+            }
+            .font(.largeTitle)
+//            .navigationTitle("Title")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .ignoresSafeArea(.all)
+//            .background(reduceTransparency ? Color.black : Color.black.opacity(0.5))
+        }
     }
 }
 
