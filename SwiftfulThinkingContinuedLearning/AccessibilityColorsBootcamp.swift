ff61ddebecd58243f2918761fc180f639c68f7ea
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-20 07:31:30 +0100

    [Chore] Create AccessibilityColorosBoootcamp

---
+++ SwiftfulThinkingContinuedLearning/AccessibilityColorsBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  AccessibilityColorsBootcamp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by Thomas on 1/20/25.
+//
+
+import SwiftUI
+
+struct AccessibilityColorsBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AccessibilityColorsBootcamp()
+}
